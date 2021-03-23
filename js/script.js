df8109d51913fbfc412e2f5579031f4bc02b418e
kai <sea.k.naka@gmail.com>
2021-03-23 17:49:47 +0900

    third commit

--- js/script.js
+++ js/script.js
@@ -4,5 +4,17 @@ $(function () {
   $('dd:not(:first)').hide();
   $('.faq-list dt').click(function() {
     $(this).next('dd').slideToggle()
-  })
+  });
+
+  var swiper = new Swiper('.swiper-container', {
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    slidesPerView: 3.5,
+    spaceBetween: 40,
+    initialSlide: 1,
+    // centeredSlides: true,
+  });
 });
