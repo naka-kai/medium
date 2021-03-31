46b04a19f2837a75215148accc466efe86d55497
kai <sea.k.naka@gmail.com>
2021-03-31 09:58:20 +0900

    eleventh commit

--- js/script.js
+++ js/script.js
@@ -14,35 +14,14 @@ $(function () {
       delay: 5000,
       disableOnInteraction: false
     },
-    breakpoints: {
-
-      // 320px以上の場合
-      320: {
-        slidesPerView: 1,
-        spaceBetween: 20,
-      },
-      // 768px以上の場合
-      768: {
-        slidesPerView: 2,
-        spaceBetween: 20,
-      },
-      // 980px以上の場合
-      1000: {
-        slidesPerView: 2,
-        spaceBetween: 20,
-      },
-      // 1200px以上の場合
-      1200: {
-        slidesPerView: 3.5,
-        spaceBetween: 40,
-      },
-    },
+    slidesPerView: 'auto',
     spaceBetween: 40,
     initialSlide: 1,
   });
 
+
   // お問い合わせへのスムーススクロール
-  var headerHight = 80;
+  var headerHight = 94;
   $('a[href^="#"]').click(function () {
     let speed = 500;
     let href = $(this).attr("href");
@@ -56,6 +35,36 @@ $(function () {
   $('.ham').on('click', function () {
     $('.header-sp-nav, .ham, .ham-list1, .ham-list2, .ham-list3').toggleClass('open');
   })
+
+  //お問い合わせフォームのバリデーション
+  //始めにjQueryで送信ボタンを無効化する
+  $('#submit').prop("disabled", true);
+  //始めにjQueryで必須欄を加工する
+  $('form input:required').each(function () {
+    $(this).prev("label").addClass("required");
+  });
+  //入力欄の操作時
+  $('form input,textarea:required').change(function () {
+    //必須項目が空かどうかフラグ
+    let flag = true;
+    //必須項目をひとつずつチェック
+    $('form input:required').each(function (e) {
+      //もし必須項目が空なら
+      if ($('form input,textarea:required').eq(e).val() === "") {
+        flag = false;
+      }
+    });
+    //全て埋まっていたら
+    if (flag) {
+      //送信ボタンを復活
+      $('#submit').prop("disabled", false);
+    }
+    else {
+      //送信ボタンを閉じる
+      $('#submit').prop("disabled", true);
+    }
+  });
+
 
 
 
